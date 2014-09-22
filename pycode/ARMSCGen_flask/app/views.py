d756fdd661fbef66055ba1c29f74514c839763fa
alex.park <saintlinu+git@gmail.com>
2014-09-23 07:23:57 +0900

    add some css files

--- pycode/ARMSCGen_flask/app/views.py
+++ pycode/ARMSCGen_flask/app/views.py
@@ -4,4 +4,4 @@ from flask import render_template, flash, redirect, session, url_for, request, g
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title = 'Home', user = user, posts = posts)
+    return render_template('index.html', title = 'Home')
