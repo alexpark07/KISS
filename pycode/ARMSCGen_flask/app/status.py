5c440739572f74ec5520e503a815bac9b170105e
alex.park <saintlinu+git@gmail.com>
2014-09-24 06:38:33 +0900

    added shellcode routing

---
+++ pycode/ARMSCGen_flask/app/status.py
@@ -0,0 +1,23 @@
+from app import app
+from ARMSCGen import *
+
+def getStatus():
+
+    sc = {}
+    sc['thumb'] = getShellcodeInfo('thumb')
+    sc['arm']   = getShellcodeInfo('arm')
+    sc['arm64'] = getShellcodeInfo('arm64')
+    
+    result = '''
+by arch
+-------
+<a href="/thumb">Thumb</a> Mode: %s
+<a href="/arm">ARM</a>   Mode: %s
+<a href="/arm64">ARM64</a> Mode: %s''' % (len(sc['thumb']), len(sc['arm']), len(sc['arm64']))
+
+    result += '''
+
+by list
+-------'''
+    sc['html'] = result    
+    return sc
--- pycode/ARMSCGen_flask/app/views.py
+++ pycode/ARMSCGen_flask/app/views.py
@@ -2,99 +2,70 @@ from app import app
 from flask import render_template, flash, redirect, session, url_for, request, g
 
 from ARMSCGen import *
+from app import status
 
 @app.route('/')
 @app.route('/index')
 def index():
+    sc = status.getStatus()
 
-    sc = {}
-    sc['thumb'] = getShellcodeInfo('thumb')
-    sc['arm']   = getShellcodeInfo('arm')
-    sc['arm64'] = getShellcodeInfo('arm64')
-    
-    result = '''
-by arch
--------
-<a href="/thumb">Thumb</a> Mode: %s
-<a href="/arm">ARM</a>   Mode: %s
-<a href="/arm64">ARM64</a> Mode: %s''' % (len(sc['thumb']), len(sc['arm']), len(sc['arm64']))
-
-    result += '''
-
-by list
--------'''
     return render_template('index.html', 
         title = 'Home', 
-        result = result, 
+        result = sc['html'], 
         thumb = sc['thumb'],
         arm = sc['arm'],
         arm64 = sc['arm64'])
 
+@app.route('/thumb/<scodename>')
 @app.route('/thumb')
-def thumb_func():
-    sc = {}
-    sc['thumb'] = getShellcodeInfo('thumb')
-    sc['arm']   = getShellcodeInfo('arm')
-    sc['arm64'] = getShellcodeInfo('arm64')
-    
-    result = '''
-by arch
--------
-<a href="/thumb">Thumb</a> Mode: %s
-<a href="/arm">ARM</a>   Mode: %s
-<a href="/arm64">ARM64</a> Mode: %s''' % (len(sc['thumb']), len(sc['arm']), len(sc['arm64']))
+def thumb_func(scodename=None):
+    if scodename is None:
+        sc = status.getStatus()
 
-    result += '''
-
-by list
--------'''
-    return render_template('thumb.html', 
-        title = 'Thumb Mode', 
-        result = result, 
-        thumb = sc['thumb'])
+        return render_template('thumb.html', 
+            title = 'Thumb Mode', 
+            result = sc['html'], 
+            thumb = sc['thumb'])
+    else:
+        show = getShellcodeHelp(scodename, 'thumb')
+        return render_template('shellcodeInfo.html', 
+            title = 'Thumb Mode', 
+            scodename = scodename,
+            mode  = 'thumb',
+            result = show)
 
 @app.route('/arm')
-def arm_func():
-    sc = {}
-    sc['thumb'] = getShellcodeInfo('thumb')
-    sc['arm']   = getShellcodeInfo('arm')
-    sc['arm64'] = getShellcodeInfo('arm64')
-    
-    result = '''
-by arch
--------
-<a href="/thumb">Thumb</a> Mode: %s
-<a href="/arm">ARM</a>   Mode: %s
-<a href="/arm64">ARM64</a> Mode: %s''' % (len(sc['thumb']), len(sc['arm']), len(sc['arm64']))
+@app.route('/arm/<scodename>')
+def arm_func(scodename=None):
+    if scodename is None:
+        sc = status.getStatus()
 
-    result += '''
-
-by list
--------'''
-    return render_template('arm.html', 
-        title = 'ARM Mode', 
-        result = result, 
-        arm = sc['arm'])
+        return render_template('arm.html', 
+            title = 'ARM Mode', 
+            result = sc['html'], 
+            arm = sc['arm'])
+    else:
+        show = getShellcodeHelp(scodename, 'arm')
+        return render_template('shellcodeInfo.html', 
+            title = 'ARM Mode', 
+            scodename = scodename,
+            mode  = 'arm',
+            result = show)
 
 @app.route('/arm64')
-def arm64_func():
-    sc = {}
-    sc['thumb'] = getShellcodeInfo('thumb')
-    sc['arm']   = getShellcodeInfo('arm')
-    sc['arm64'] = getShellcodeInfo('arm64')
-    
-    result = '''
-by arch
--------
-<a href="/thumb">Thumb</a> Mode: %s
-<a href="/arm">ARM</a>   Mode: %s
-<a href="/arm64">ARM64</a> Mode: %s''' % (len(sc['thumb']), len(sc['arm']), len(sc['arm64']))
+@app.route('/arm64/<scodename>')
+def arm64_func(scodename=None):
+    if scodename is None:
+        sc = status.getStatus()
 
-    result += '''
-
-by list
--------'''
-    return render_template('arm64.html', 
-        title = 'ARM64 Mode', 
-        result = result, 
-        arm64 = sc['arm64'])
+        return render_template('arm64.html', 
+            title = 'ARM64 Mode', 
+            result = sc['html'], 
+            arm64 = sc['arm64'])
+    else:
+        show = getShellcodeHelp(scodename, 'arm64')
+        return render_template('shellcodeInfo.html', 
+            title = 'ARM64 Mode', 
+            scodename = scodename,
+            mode  = 'arm64',
+            result = show)
